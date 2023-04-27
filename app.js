426bf3b40b245cd1b29e3b2fff910bd1640a303a
Shivam Sharma <shivampramod95925@gmail.com>
2023-04-27 16:12:21 -0500

    grabing all required html element

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
-//all mandatory html button and div
+//grabing all required html element
 
-const wrongltr=document.getElementById('wrong-letters');
-const word =document.getElementById('word');
-const popupcntner=document.getElementById('popup-container');
-const notifycntner=document.getElementById('notification-container');
-const playbutton=document.getElementById('button');
-const finalmsg=document.getElementById('final-massage');
-const figureparts=document.querySelectorAll('.figure-part');
+const wrongltr = document.getElementById("wrong-letters");
+const word = document.getElementById("word");
+const popupcntner = document.getElementById("popup-container");
+const notifycntner = document.getElementById("notification-container");
+const playbutton = document.getElementById("button");
+const finalmsg = document.getElementById("final-massage");
+const figureparts = document.querySelectorAll(".figure-part");
 
 //new array of words of game
-const words = ["application","programming","interface","wizard"];
+const words = ["application", "programming", "interface", "wizard"];
 
 // render word of game
 let selectedword = words[Math.floor(Math.random() * words.length)];
@@ -21,110 +21,109 @@ let correctletter = [];
 let worngletter = [];
 
 //display function user input key display
-function displayword(){
-  
+function displayword() {
   //correct word display
-  word.innerHTML=`${selectedword
-  .split('')
-  .map(letr=>`<span class="letters">
-  ${correctletter.includes(letr) ? letr : ''}
+  word.innerHTML = `${selectedword
+    .split("")
+    .map(
+      (letr) => `<span class="letters">
+  ${correctletter.includes(letr) ? letr : ""}
   </span>
-     `).join('')
-    
-    }
+     `
+    )
+    .join("")}
  `;
- // conversation of vertical multi line words to horizontal  single line words
- const innerword = word.innerText.replace(/\n/g,'');
- //winning condition
- if(innerword == selectedword) {
-   finalmsg.innerText='Congratulation You Won!';
-   popupcntner.style.display= 'flex';
- }
+  // conversation of vertical multi line words to horizontal  single line words
+  const innerword = word.innerText.replace(/\n/g, "");
+  //winning condition
+  if (innerword == selectedword) {
+    finalmsg.innerText = "Congratulation You Won!";
+    popupcntner.style.display = "flex";
+  }
 }
 
 // wrong letter update function
- function updateWrongLetter(){
+function updateWrongLetter() {
   //wrong letter display
-   wrongltr.innerHTML=`${worngletter.length > 0 ? "<p>wrong letter</p>" : ''}
-     ${worngletter.map(letter => `<span>${letter}</span>`)
-     }
+  wrongltr.innerHTML = `${worngletter.length > 0 ? "<p>wrong letter</p>" : ""}
+     ${worngletter.map((letter) => `<span>${letter}</span>`)}
    `;
-   //body part display one by one
-   figureparts.forEach((part,index)=>{
-     const error = worngletter.length;
-     
-     if (index < error) {
-       part.style.display='block'
-     }else{
-       part.style.display='none'
-     }
-   });
-   
+  //body part display one by one
+  figureparts.forEach((part, index) => {
+    const error = worngletter.length;
+
+    if (index < error) {
+      part.style.display = "block";
+    } else {
+      part.style.display = "none";
+    }
+  });
+
   //lose check
-   if(worngletter.length === figureparts.length){
-     finalmsg.innerText= "Men dead!! You lose"
-     popupcntner.style.display= 'flex';
-   }
- }
- 
- // notify function //already entered many times
- function showNotification(){
-   //adding show class to notification-container
-   notifycntner.classList.add('show');
-   //removing show class from notification-container
-   setTimeout(function() {
-     notifycntner.classList.remove('show');
-   }, 2000);
- }
- 
- //window event listener
- window.addEventListener('keydown', e => {
-   //pressed small a to z key code
-   if (e.keyCode >= 60 && e.keyCode <= 90) {
-     let letter = e.key;
-     
-     //correction of entered word checkup
-     if (selectedword.includes(letter)) {
-       //addind word correctletter array if word not already exists
-       if(!correctletter.includes(letter)){
-         correctletter.push(letter);
-         
-         displayword();
-       }else{
-         //showing notification-container
-         showNotification();
-       }
-     }else{
-       //add entered word to wrong array if  word not initialy exists 
-       if (!worngletter.includes(letter)) {
-         worngletter.push(letter);
-         //updating wrong letter
-         updateWrongLetter();
-       }else{
-         //showing notification-container
-         showNotification();
-       }
-     }
-   }
- });
- 
- //playbutton listener
- playbutton.addEventListener('click', ()=>{
-   //deleting intial cotebt present in correctletter and worngletter array
-   correctletter.splice(0);
-   worngletter.splice(0);
-   
-   //regenerating words 
-   selectedword = words[Math.floor(Math.random() * words.length)];
-   
-   //call word display
-   displayword();
-   
-   //updating updateWrongLetter
-   updateWrongLetter();
-   
-  // hiding popupcntner 
-   popupcntner.style.display="none";
- });
+  if (worngletter.length === figureparts.length) {
+    finalmsg.innerText = "Men dead!! You lose";
+    popupcntner.style.display = "flex";
+  }
+}
+
+// notify function //already entered many times
+function showNotification() {
+  //adding show class to notification-container
+  notifycntner.classList.add("show");
+  //removing show class from notification-container
+  setTimeout(function () {
+    notifycntner.classList.remove("show");
+  }, 2000);
+}
+
+//window event listener
+window.addEventListener("keydown", (e) => {
+  //pressed small a to z key code
+  if (e.keyCode >= 60 && e.keyCode <= 90) {
+    let letter = e.key;
+
+    //correction of entered word checkup
+    if (selectedword.includes(letter)) {
+      //addind word correctletter array if word not already exists
+      if (!correctletter.includes(letter)) {
+        correctletter.push(letter);
+
+        displayword();
+      } else {
+        //showing notification-container
+        showNotification();
+      }
+    } else {
+      //add entered word to wrong array if  word not initialy exists
+      if (!worngletter.includes(letter)) {
+        worngletter.push(letter);
+        //updating wrong letter
+        updateWrongLetter();
+      } else {
+        //showing notification-container
+        showNotification();
+      }
+    }
+  }
+});
+
+//playbutton listener
+playbutton.addEventListener("click", () => {
+  //deleting intial cotebt present in correctletter and worngletter array
+  correctletter.splice(0);
+  worngletter.splice(0);
+
+  //regenerating words
+  selectedword = words[Math.floor(Math.random() * words.length)];
+
+  //call word display
+  displayword();
+
+  //updating updateWrongLetter
+  updateWrongLetter();
+
+  // hiding popupcntner
+  popupcntner.style.display = "none";
+});
 
 displayword();
